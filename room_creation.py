d11ac0be346d75d5b3b295e29f16b633100d6fec
vpara389 <psvarsha98@gmail.com>
2026-05-08 17:26:04 -0600

    Made the floor and room number dynamic to accept user input

--- room_creation.py
+++ room_creation.py
@@ -8,8 +8,8 @@ filename = "Room_Report.csv"
 with open (filename, "w", newline="") as file:
     writer = csv.DictWriter(file, fieldnames=columns)
     writer.writeheader()
-    for floor in range(1, 5):
-        for room in range(1, 39):
+    for floor in range(int(input("Enter the starting floor number: ")), int(input("Enter the ending floor number: "))+1):
+        for room in range(int(input("Enter the starting room number: ")), int(input("Enter the ending room number: "))+1):
             room_number = f'{floor}{room:02d}'
             r_type = random.choice(room_types)
             writer.writerow({"Room_Number": room_number, "Room_Type": r_type})
